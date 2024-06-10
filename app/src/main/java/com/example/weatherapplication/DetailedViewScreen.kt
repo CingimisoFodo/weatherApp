3d2de9a6318c314cbbf5b83f637077ebd27f3324
CingimisoFodo <ST10402125@vcconnect.edu.za>
2024-06-10 12:02:48 +0200

    first commit
    first commit

---
+++ app/src/main/java/com/example/weatherapplication/DetailedViewScreen.kt
@@ -0,0 +1,16 @@
+package com.example.weatherapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailedViewScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detailed_view_screen)
+
+        }
+    }
